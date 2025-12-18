51afec03dc13ed915f89ded8f2dcc61c716b8998
Moiz <94140739+iKxjiz@users.noreply.github.com>
2025-12-18 11:42:03 +0100

    Update image gallery in README

--- Figure/README.m
+++ Figure/README.m
@@ -10,40 +10,40 @@ Le immagini sono generate tramite gli script presenti nel repository e raccolte 
 ## Galleria di figure
 
 <table>
-<tr>
-<td><img src="Immagini/smdppbez_interp_anelli_intersecati.png" width="400"/></td>
-<td><img src="Immagini/smdppbez_interp_ceco.png" width="400"/></td>
-</tr>
-<tr>
-<td><img src="Immagini/smdppbez_interp_cuore_con_rettangolo_lituania.png" width="400"/></td>
-<td><img src="Immagini/smdppbez_interp_cuore_france.png" width="400"/></td>
-</tr>
-<tr>
-<td><img src="Immagini/smdppbez_interp_finlandia.png" width="400"/></td>
-<td><img src="Immagini/smdppbez_interp_fiore.png" width="400"/></td>
-</tr>
-<tr>
-<td><img src="Immagini/smdppbez_interp_germ.png" width="400"/></td>
-<td><img src="Immagini/smdppbez_interp_girandola.png" width="400"/></td>
-</tr>
-<tr>
-<td><img src="Immagini/smdppbez_interp_italy.png" width="400"/></td>
-<td><img src="Immagini/smdppbez_interp_latvia.png" width="400"/></td>
-</tr>
-<tr>
-<td><img src="Immagini/smdppbez_interp_marchingegno.png" width="400"/></td>
-<td><img src="Immagini/smdppbez_interp_petali.png" width="400"/></td>
-</tr>
-<tr>
-<td><img src="Immagini/smdppbez_interp_polipo.png" width="400"/></td>
-<td><img src="Immagini/smdppbez_interp_quadri.png" width="400"/></td>
-</tr>
-<tr>
-<td><img src="Immagini/smdppbez_interp_scudo_fiore_giallo.png" width="400"/></td>
-<td><img src="Immagini/smdppbez_interp_scudo.png" width="400"/></td>
-</tr>
-<tr>
-<td><img src="Immagini/smdppbez_interp_segnale.png" width="400"/></td>
-<td></td>
-</tr>
+  <tr>
+    <td><img src="Immagini/smdppbez_interp_anelli_intersecati.png" width="400"/></td>
+    <td><img src="Immagini/smdppbez_interp_ceco.png" width="400"/></td>
+  </tr>
+  <tr>
+    <td><img src="Immagini/smdppbez_interp_cuore_con_rettangolo_lituania.png" width="400"/></td>
+    <td><img src="Immagini/smdppbez_interp_cuore_france.png" width="400"/></td>
+  </tr>
+  <tr>
+    <td><img src="Immagini/smdppbez_interp_finlandia.png" width="400"/></td>
+    <td><img src="Immagini/smdppbez_interp_fiore.png" width="400"/></td>
+  </tr>
+  <tr>
+    <td><img src="Immagini/smdppbez_interp_germ.png" width="400"/></td>
+    <td><img src="Immagini/smdppbez_interp_girandola.png" width="400"/></td>
+  </tr>
+  <tr>
+    <td><img src="Immagini/smdppbez_interp_italy.png" width="400"/></td>
+    <td><img src="Immagini/smdppbez_interp_latvia.png" width="400"/></td>
+  </tr>
+  <tr>
+    <td><img src="Immagini/smdppbez_interp_marchingegno.png" width="400"/></td>
+    <td><img src="Immagini/smdppbez_interp_petali.png" width="400"/></td>
+  </tr>
+  <tr>
+    <td><img src="Immagini/smdppbez_interp_polipo.png" width="400"/></td>
+    <td><img src="Immagini/smdppbez_interp_quadri.png" width="400"/></td>
+  </tr>
+  <tr>
+    <td><img src="Immagini/smdppbez_interp_scudo_fiore_giallo.png" width="400"/></td>
+    <td><img src="Immagini/smdppbez_interp_scudo.png" width="400"/></td>
+  </tr>
+  <tr>
+    <td><img src="Immagini/smdppbez_interp_segnale.png" width="400"/></td>
+    <td></td>
+  </tr>
 </table>
