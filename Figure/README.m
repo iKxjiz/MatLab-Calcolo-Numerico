59270cd0894e4adfc5cfb60325d72011f852125c
Moiz <moizahmad874@gmail.com>
2025-12-18 11:38:10 +0100

    Aggiunto il README.m

---
+++ Figure/README.m
@@ -0,0 +1,49 @@
+# Raccolta di figure – SMDPP / Bézier
+
+Ho condiviso questi appunti e queste figure gratuitamente.
+La loro realizzazione ha richiesto diverse ore di lavoro; se li trovassi utili, ti chiederei di lasciare una ⭐ alla repository come supporto e riconoscimento.
+
+Le immagini sono generate tramite gli script presenti nel repository e raccolte nella cartella `Immagini/`.
+
+---
+
+## Galleria di figure
+
+<table>
+<tr>
+<td><img src="Immagini/smdppbez_interp_anelli_intersecati.png" width="400"/></td>
+<td><img src="Immagini/smdppbez_interp_ceco.png" width="400"/></td>
+</tr>
+<tr>
+<td><img src="Immagini/smdppbez_interp_cuore_con_rettangolo_lituania.png" width="400"/></td>
+<td><img src="Immagini/smdppbez_interp_cuore_france.png" width="400"/></td>
+</tr>
+<tr>
+<td><img src="Immagini/smdppbez_interp_finlandia.png" width="400"/></td>
+<td><img src="Immagini/smdppbez_interp_fiore.png" width="400"/></td>
+</tr>
+<tr>
+<td><img src="Immagini/smdppbez_interp_germ.png" width="400"/></td>
+<td><img src="Immagini/smdppbez_interp_girandola.png" width="400"/></td>
+</tr>
+<tr>
+<td><img src="Immagini/smdppbez_interp_italy.png" width="400"/></td>
+<td><img src="Immagini/smdppbez_interp_latvia.png" width="400"/></td>
+</tr>
+<tr>
+<td><img src="Immagini/smdppbez_interp_marchingegno.png" width="400"/></td>
+<td><img src="Immagini/smdppbez_interp_petali.png" width="400"/></td>
+</tr>
+<tr>
+<td><img src="Immagini/smdppbez_interp_polipo.png" width="400"/></td>
+<td><img src="Immagini/smdppbez_interp_quadri.png" width="400"/></td>
+</tr>
+<tr>
+<td><img src="Immagini/smdppbez_interp_scudo_fiore_giallo.png" width="400"/></td>
+<td><img src="Immagini/smdppbez_interp_scudo.png" width="400"/></td>
+</tr>
+<tr>
+<td><img src="Immagini/smdppbez_interp_segnale.png" width="400"/></td>
+<td></td>
+</tr>
+</table>
